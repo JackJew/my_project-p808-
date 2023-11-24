f6bc0e00c97ac6128d6746adb486402411ae04d8
root <root@DESKTOP-2C5MO4R.localdomain>
2023-11-24 14:38:06 +0300

    product app created

--- app/contact/views.py
+++ app/contact/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import redirect, render 
+from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from django.contrib import messages
 
-
 from .forms import AppealingForm
 from .models import ContactInfo
+
 
 def contact(request):
     contact_info = ContactInfo.objects.first()
@@ -13,15 +13,11 @@ def contact(request):
         form = AppealingForm(request.POST)
         if form.is_valid():
             form.save()
-            messages.success(request,' Мы приняли ваш запрос, постараемся ответить как можно быстрее)')
-            
+            messages.success(request, 'Müraciətiniz uğurla qeydə alındı, tezliklə sizinlə əlaqə saxlanılacaq')
             return redirect(reverse_lazy('contact'))
 
-    context  = {
-        "contact_info" : contact_info,
-        'form' : form
+    context = {
+        'contact_info': contact_info,
+        'form': form
     }
-    
-    return render(request, 'contact/index.html ', context)
-
- 
+    return render(request, 'contact/index.html', context)
--- app/core/context_processors/context_processors.py
+++ app/core/context_processors/context_processors.py
@@ -1,9 +1,10 @@
 from product.models import Category
+from contact.models import ContactInfo
 
 
-def subject_render(request):
+def subject_renderer(request):
     context = {
-        'context_categories': Category.objects.filter(is_parent=True)
-        
+        'context_categories': Category.objects.filter(is_parent=True),
+        'context_contact': ContactInfo.objects.first()
     }
     return context
